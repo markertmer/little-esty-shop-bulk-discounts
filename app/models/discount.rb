ced5a46fb0ac40ec9845767f04bdb856c4ce6f25
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-04 13:07:08 -0700

    Add: model validations & tests thereof

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,10 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :name
+  validates_presence_of :percent
+  validates_presence_of :threshold
+
+  validates :percent, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :threshold, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Discount, type: :model do
   describe "relationships" do
     it { should belong_to(:merchant) }
   end
+
+  describe "attributes" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:percent) }
+    it { should validate_presence_of(:threshold) }
+
+    it { should validate_numericality_of(:percent).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:percent).is_less_than_or_equal_to(100) }
+    it { should validate_numericality_of(:threshold).is_greater_than_or_equal_to(0) }
+  end
 end
